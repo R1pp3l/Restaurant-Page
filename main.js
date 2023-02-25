0e18bcd8ff34f1b39c323f38284197ba2c1c0725
Victor Cabral <victorcabralweb@outlook.com>
2023-02-25 16:52:03 +0800

    import openTab

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import openTab from "./switch";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i += 1) {
+    tablinks[i].addEventListener("click", openTab);
+  }
+
+  document.querySelector('.tablinks[data-tab="contentOne"]').click();
+});
